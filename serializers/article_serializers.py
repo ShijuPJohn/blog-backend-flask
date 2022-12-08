125b60a702ac08e63b360b32c6e1d07c3ee203b6
ShijuPJohn <shijupjohn4u@gmail.com>
2022-12-08 13:55:27 +0530

    finish article serializers. untested

--- serializers/article_serializers.py
+++ serializers/article_serializers.py
@@ -3,11 +3,16 @@ from marshmallow import Schema, fields
 from serializers.user_serializers import UserSchema
 
 
+class CategorySchema(Schema):
+    id = fields.Integer()
+    name = fields.String(required=True)
+
+
 class ArticleSchema(Schema):
     id = fields.Integer()
     title = fields.String(required=True)
     post = fields.String(required=True)
     author = fields.Nested(UserSchema)
     approved_by = fields.Nested(UserSchema)
-    category = fields.Nested(db.ForeignKey(Category.id), nullable=False)
+    category = fields.Nested(CategorySchema, required=True)
     created_time = fields.DateTime()
