b19330883b86588900a79814fe5ac0b6ffb3aba3
ShijuPJohn <shijupjohn4u@gmail.com>
2022-12-12 00:05:25 +0530

    remove debug true

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 db.init_app(app)
 from controllers.user_controllers import *
+
 # from controllers.articles_controllers import *
 if __name__ == "__main__":
     with app.app_context():
-        app.run(debug=True if os.environ.get("ENV") == "DEVELOPMENT" else False, port=int(os.environ.get("PORT", 8080)))
+        app.run(port=int(os.environ.get("PORT", 8080)))
