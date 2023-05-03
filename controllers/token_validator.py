ebae935fe846308381dfec7f09ae6d31bd16f79c
ShijuPJohn <shijupjohn4u@gmail.com>
2023-05-03 21:49:34 +0530

    change .gitignore to add token_validator.py

---
+++ controllers/token_validator.py
@@ -0,0 +1,29 @@
+import jwt
+from flask import request, jsonify, Blueprint
+
+from models.models import User
+from flask import current_app as app
+# token_validator = Blueprint('token_validator', __name__)
+
+
+def validate_token(func):
+    def w_func(*args, **kwargs):
+        token = None
+        if "x-token" in request.headers:
+            token = request.headers["x-token"]
+        if not token:
+            return jsonify({"message": "token_absent"}), 401
+        try:
+            decoded_data = jwt.decode(token, app.config["SECRET_KEY"], algorithms=['HS256'])
+            user_id_from_token = decoded_data["user_id"]
+            user = User.query.filter(User.id == user_id_from_token).first()
+            kwargs["user_from_token"] = user
+            val = func(*args, **kwargs)
+        except Exception as e:
+            print(e)
+            return jsonify({"message": "invalid_token"}), 401
+        return val
+
+    w_func.__name__ = func.__name__
+
+    return w_func
